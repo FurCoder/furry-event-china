78697d95c6a79de97b3f3c9464c011c206699265
JiPai <github@jipai.moe>
2023-02-12 05:11:21 +0800

    refactor(eventCard): now eventCard is a component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import { useMemo } from "react";
 import { groupBy } from "lodash-es";
 import { XataClient, Event } from "@/xata/xata";
+import EventCard from "@/components/eventCard";
 
 export default function Home(props: { events: Event[] }) {
   const groupByCustomDurationEvent = useMemo(() => {
@@ -116,39 +117,7 @@ export async function getStaticProps() {
   };
 }
 
-function EventCard({ event }: { event: Event }) {
-  return (
-    <Link href={`/${event.organization?.slug}/${event.slug}`}>
-      <div className="bg-white rounded-xl h-96 relative group">
-        <div
-          className={`flex flex-col justify-end h-full rounded-xl ${styles["event-card"]}`}
-          style={{
-            backgroundImage: `url(${event.coverUrl})`,
-            backgroundPosition: "center",
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-          }}
-        >
-          <div className="p-6 bg-gray-400/25 group-hover:bg-gray-400/50 transition duration-300 rounded-b-xl">
-            <h1 className="text-white font-bold text-2xl">
-              {event.organization?.name}·{event.name}
-            </h1>
-            <h2 className="text-white font-bold text-xl">{event.city}市</h2>
-            <div className="text-white text-base">
-              {event.startDate && event.endDate
-                ? `${new Date(event.startDate).toLocaleDateString()} - 
-              ${new Date(event.endDate).toLocaleDateString()}`
-                : null}
-              <span></span>
-              <br />
-              <span>{event.address}</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </Link>
-  );
-}
+
 enum DurationType {
   Passed = "passed", //already done.
   Now = "now", // in the duration of event.
