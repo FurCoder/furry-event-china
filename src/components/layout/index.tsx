b3e5aa00ab04aec116a064701ecbf439314e784f
JiPai <github@jipai.moe>
2023-02-11 19:14:10 +0800

    style(layout): make sure footer always in the bottom of page

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -5,14 +5,14 @@ import Footer from "@/components/layout/footer";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="sm:max-w-screen-lg mx-auto">
+    <div className="sm:max-w-screen-lg mx-auto flex flex-col min-h-screen">
       <Head>
         <meta name="description" content="小动物们的展子日历" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      {children}
+      <div className="flex-grow">{children}</div>
       <Footer />
     </div>
   );
