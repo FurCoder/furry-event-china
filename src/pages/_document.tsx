9ed0baf5a9f2447a38d6796439f8b72223500728
JiPai <github@jipai.moe>
2023-02-11 16:16:56 +0800

    chore(track): add ga4 and umami track script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -8,6 +9,23 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Script
+          strategy="lazyOnload"
+          src="https://www.googletagmanager.com/gtag/js?id=G-RBND7XQ43D"
+        />
+        <Script id="ga" strategy="lazyOnload">
+          {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-RBND7XQ43D');
+                `}
+        </Script>
+        <Script
+          data-website-id="a12ca72b-5704-4910-bb28-9dd09d576e91"
+          strategy="lazyOnload"
+          src="https://analytics.umami.is/script.js"
+        />
       </body>
     </Html>
   );
