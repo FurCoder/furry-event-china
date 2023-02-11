07f9a1638836e0c7f9dac98a168d6afb525e72b4
JiPai <github@jipai.moe>
2023-02-11 21:24:00 +0800

    feat(pages): add organization page

--- src/pages/organization.tsx
+++ src/pages/organization.tsx
@@ -1,7 +1,47 @@
-export default function Organization() {
+import { Organization, XataClient } from "@/xata/xata";
+import Link from "next/link";
+import { groupBy } from "lodash-es";
+import Image from "next/image";
+
+export default function OrganizationPage({
+  organizations,
+}: {
+  organizations: Organization[];
+}) {
+  const groupByStatusOrganizations = groupBy(organizations, (o) => o.status);
+
   return (
-    <div className="bg-white p-6 rounded-xl text-center font-bold text-gray-600 text-xl">
-      🐦 鸽子在码头施工中
+    <div className="bg-white p-6 rounded-xl">
+      <section>
+        <h1 className="text-bold text-gray-800 text-xl">活跃展商</h1>
+        <div className="mt-4 grid md:grid-cols-3 gap-10">
+          {groupByStatusOrganizations["active"].map((o) => (
+            <OrganizationItem key={o.id} organization={o} />
+          ))}
+        </div>
+      </section>
     </div>
   );
+}
+
+function OrganizationItem({ organization }: { organization: Organization }) {
+  return (
+    <Link href={organization.slug || ""}>
+      <div className="rounded-xl border p-6 cursor-pointer h-full flex flex-row items-center md:flex-col">
+        <div className="relative w-2/4 md:h-3/4 max-h-12"><Image className="object-contain h-full max-h-12" src={organization.logoUrl || ""} alt={`${organization.name}'s logo`} width={300} height={50} /></div>
+        <h2 className="w-2/4 tracking-wide text-gray-600 md:text-center text-lg border-l md:border-l-0 ml-4 md:ml-0 pl-4 md:pl-0 md:mt-4 font-bold">{organization.name}</h2>
+      </div>
+    </Link>
+  );
+}
+
+export async function getStaticProps() {
+  const xata = new XataClient();
+
+  const organizations = await xata.db.organization.select(["*"]).getAll();
+  return {
+    props: {
+      organizations,
+    },
+  };
 }
