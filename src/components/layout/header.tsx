0bb0338c312a035f2dbb4040efc784bccf91636a
JiPai <github@jipai.moe>
2023-02-11 21:23:41 +0800

    style(header): highlight current nav link

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,15 +1,16 @@
+import clsx from "clsx";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useMemo } from "react";
 
 export default function Header() {
-  const { pathname, query,route } = useRouter();
+  const { pathname } = useRouter();
+
   return (
     <header className="mb-6 rounded-b-xl bg-white flex justify-between items-center1">
       <div className="m-6 text-4xl text-geraldine font-bold">
         <span className="block">FEC 🐺🐱🐯</span>
-        <span className="text-base mt-0 block">
-          毛茸茸展会目录
-        </span>
+        <span className="text-base mt-0 block">毛茸茸展会目录</span>
       </div>
       <nav className="mr-6 hidden md:block">
         <ol className="flex h-full">
@@ -21,7 +22,10 @@ export default function Header() {
           ].map((nav) => (
             <li
               key={nav.name}
-              className="relative font-bold flex items-center mx-4 text-red-400 after:transition-all after:ease-out after:duration-300 hover:bg-red-2001 hover:text-white1 cursor-pointer after:absolute after:w-full after:content-[''] hover:after:h-2 after:h-0 after:bottom-0 after:bg-red-200"
+              className={clsx(
+                "relative font-bold flex items-center mx-4 text-red-400 after:transition-all after:ease-out after:duration-300 hover:bg-red-2001 hover:text-white1 cursor-pointer after:absolute after:w-full after:content-[''] hover:after:h-2 after:h-0 after:bottom-0 after:bg-red-200",
+                pathname === nav.link && "after:h-2"
+              )}
             >
               <Link className="px-6 py-2 inline-block" href={nav.link}>
                 {nav.name}
