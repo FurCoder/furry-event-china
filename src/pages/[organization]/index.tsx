07a5e10164f7ec8e8e4974984025cff21f8e04f8
JiPai <github@jipai.moe>
2023-02-12 05:12:56 +0800

    feat(organization-detail): add basic organization detail page

--- src/pages/[organization]/index.tsx
+++ src/pages/[organization]/index.tsx
@@ -1,63 +1,146 @@
-import { Organization, XataClient } from "@/xata/xata";
+import { Organization, XataClient, Event } from "@/xata/xata";
 import Image from "next/image";
 import { GrStatusGoodSmall } from "react-icons/gr";
-import { FiMail } from "react-icons/fi";
+import { HiOutlineMail } from "react-icons/hi";
 import { FaBirthdayCake } from "react-icons/fa";
+import { MdOutlineContentCopy } from "react-icons/md";
 import { GetStaticPropsContext } from "next/types";
+import { useMemo } from "react";
+import styles from "@/styles/Organization.module.css";
+import clsx from "clsx";
+import EventCard from "@/components/eventCard";
 
 const xata = new XataClient();
 export default function OrganizationDetail(props: {
+  events: Event[];
   organization: Organization;
 }) {
-  const { organization } = props;
+  const { organization, events } = props;
+
+  const formatedCreationTime = useMemo(() => {
+    if (organization.creationTime) {
+      const date = new Date(organization.creationTime);
+      console.log(organization.creationTime);
+      return {
+        year: date.getFullYear(),
+        month: date.getMonth() + 1,
+        day: date.getDate(),
+      };
+    } else {
+      return null;
+    }
+  }, [organization.creationTime]);
+
   return (
-    <div className="border bg-white rounded-xl p-6">
-      <div className="flex">
-        <div className="border rounded flex justify-center p-2" style={{ height: 100, width: 100 }}>
-          {organization.logoUrl && (
-            <Image
-              alt={`${organization.name}'s logo`}
-              width="100"
-              height="100"
-              src={organization.logoUrl}
-            />
-          )}
-        </div>
-        <div className="ml-4">
-          <h1 className="text-2xl font-bold">{organization.name}</h1>
-          <div className="flex items-center text-gray-500 mt-2">
-            <span className="flex items-center">
-              <GrStatusGoodSmall className="mr-1 text-green-400" />
-              {organization.status === "active" ? "活跃" : "不再活跃"}
-            </span>
-            <a href={`mailto:${organization.contactMail}`} className="mx-2 text-gray-500 flex items-center">
-              <FiMail className="mr-1" />
-              {organization.contactMail}
-            </a>
-            {organization.creationTime && (
-              <span className="mx-2 text-gray-500 flex items-center">
-                <FaBirthdayCake className="mr-1" />
-                首次举办于{" "}
-                {new Date(organization.creationTime).toLocaleString()}
-              </span>
+    <div>
+      <div className="border bg-white rounded-xl p-6">
+        <div className="flex">
+          <div
+            className="border rounded flex justify-center p-2"
+            style={{ height: 200, width: 200 }}
+          >
+            {organization.logoUrl && (
+              <Image
+                className="object-contain"
+                alt={`${organization.name}'s logo`}
+                width={200}
+                height={200}
+                src={organization.logoUrl}
+              />
             )}
           </div>
+          <div className="ml-4">
+            <h1 className="text-2xl font-bold mb-2">{organization.name}</h1>
+
+            <div className="flex items-center text-gray-500 mb-2">
+              <span className="flex items-center">
+                <GrStatusGoodSmall className="mr-1 text-green-400" />
+                {organization.status === "active" ? "活跃" : "不再活跃"}
+              </span>
+            </div>
+
+            <div
+              className={clsx(
+                "mb-2 flex items-center text-gray-500",
+                styles["intro-bar"]
+              )}
+            >
+              <span>已累计举办 {events.length} 场展会</span>
+              {formatedCreationTime && (
+                <span className="lex items-center">
+                  {`首次举办于 ${formatedCreationTime?.year}年${formatedCreationTime?.month}月${formatedCreationTime?.day}日`}
+                </span>
+              )}
+            </div>
+
+            {/* <div className="flex items-center text-gray-500 mt-2">
+            <a
+              href={`mailto:${organization.contactMail}`}
+              className="mr-2 text-gray-500 flex items-center inline-block"
+            >
+              <HiOutlineMail className="mr-1 text-xl" />
+              <span className="uppercase">{organization.contactMail}</span>
+            </a>
+          </div> */}
+
+            <div
+              className={clsx(
+                "flex items-center flex-wrap first:mr-0",
+                styles.links
+              )}
+            >
+              {organization.website && (
+                <a
+                  href={organization.website}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="bg-blue-300 rounded-xl px-4 py-1 text-white"
+                >
+                  去官网
+                </a>
+              )}
+              {/* <button className="bg-blue-300 rounded-xl px-4 py-1 text-white">
+              去微博
+            </button>
+            <button className="bg-blue-300 rounded-xl px-4 py-1 text-white">
+              去微信公众号
+            </button>
+            <button className="bg-blue-300 rounded-xl px-4 py-1 text-white">
+              去QQ群
+            </button> */}
+              {organization.contactMail && (
+                <div className="flex items-center bg-green-600 rounded-xl px-4 py-1 text-white">
+                  <a className="" href={`mailto:${organization.contactMail}`}>
+                    {/* <button className=""> */}
+                    发邮件给 {organization.contactMail}
+                    {/* </button> */}
+                  </a>
+                  <div
+                    onClick={() => {
+                      navigator.clipboard.writeText(organization.contactMail!);
+                    }}
+                    className="border-l pl-2 cursor-pointer"
+                  >
+                    <MdOutlineContentCopy />
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
         </div>
+
+        <div className="border-t my-8" />
+
+        <p className="text-slate-700">{organization.description}</p>
       </div>
 
-      <div className="border-t my-8" />
-
-      <p className="text-slate-700">{organization.description}</p>
-
-      {/* <div className="grid gap-4 grid-cols-3">
-        <div className="col-start-2 col-end-2 text-center">
-          <h1 className="text-2xl border">{organization.name}</h1>
-        </div>
-        <div className="">
-            <p>{organization.description}</p>
-        </div>
-        <div><span>{organization.status}</span></div>
-      </div> */}
+      {events.length && (
+        <section className="mt-8 grid gird-cols-1 gap-8">
+          {events.map((e) => (
+            <EventCard key={e.id} event={e} />
+          ))}
+        </section>
+      )}
     </div>
   );
 }
@@ -73,15 +156,30 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps(context: GetStaticPropsContext) {
-  const organization = await xata.db.organization
+  const organizationPromise = xata.db.organization
     .filter({
       slug: context.params?.organization as string,
     })
     .select(["*"])
     .getFirst();
+
+  const eventPromise = xata.db.event
+    .filter({
+      "organization.slug": context.params?.organization as string,
+    })
+    .select(["*", "organization.name", "organization.slug"])
+    .sort("startDate", "desc")
+    .getAll();
+
+  const [organization, events] = await Promise.all([
+    organizationPromise,
+    eventPromise,
+  ]);
+
   return {
     props: {
       organization,
+      events,
     },
   };
 }
