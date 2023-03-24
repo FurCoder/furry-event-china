d1c09f9f774998902fbea6f2ad0bca43603e3f60
JiPai <3956400+PaiJi@users.noreply.github.com>
2023-03-25 01:38:35 +0800

    chore(#13): disabled track for dev (#16)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,23 +20,27 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <Script
-          strategy="lazyOnload"
-          src="https://www.googletagmanager.com/gtag/js?id=G-RBND7XQ43D"
-        />
-        <Script id="ga" strategy="lazyOnload">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-RBND7XQ43D');
-          `}
-        </Script>
-        <Script
-          data-website-id="a12ca72b-5704-4910-bb28-9dd09d576e91"
-          strategy="lazyOnload"
-          src="https://analytics.umami.is/script.js"
-        />
+        {process.env.ENABLE_TRACK === "true" && (
+          <>
+            <Script
+              strategy="lazyOnload"
+              src="https://www.googletagmanager.com/gtag/js?id=G-RBND7XQ43D"
+            />
+            <Script id="ga" strategy="lazyOnload">
+              {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-RBND7XQ43D');
+        `}
+            </Script>
+            <Script
+              data-website-id="a12ca72b-5704-4910-bb28-9dd09d576e91"
+              strategy="lazyOnload"
+              src="https://analytics.umami.is/script.js"
+            />
+          </>
+        )}
       </body>
     </Html>
   );
