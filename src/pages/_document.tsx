8b45bfa6cbfab7902ac92dfb16569c34de121a06
JiPai <3956400+PaiJi@users.noreply.github.com>
2023-04-05 11:53:18 +0800

    chore: add clarity track (#22)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -39,6 +39,15 @@ export default function Document() {
               strategy="lazyOnload"
               src="https://analytics.umami.is/script.js"
             />
+            <Script id="clarity" strategy="lazyOnload">
+              {`
+                  (function(c,l,a,r,i,t,y){
+                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "gjlb269skt");
+              `}
+            </Script>
           </>
         )}
       </body>
