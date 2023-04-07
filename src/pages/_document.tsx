3a2665c7ad36db3f7bc94a2bf8e56f6b60b58a11
JiPai <github@jipai.moe>
2023-04-07 22:31:10 +0800

    feat: back clarity online

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -51,6 +51,23 @@ export default function Document() {
             {}
           </Script>
         )}
+        {isEnableTrack && (
+          <Script
+            id="clarity-script"
+            strategy="lazyOnload"
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "gjlb269skt");
+          `,
+            }}
+          >
+            {}
+          </Script>
+        )}
       </body>
     </Html>
   );
