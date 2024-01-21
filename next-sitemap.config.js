963fd336a5f8ae195895b808d8e61a9178988679
JiPai <github@jipai.moe>
2024-01-22 00:48:57 +0800

    feat(docker): add cn docker

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,17 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: "https://www.furryeventchina.com",
-  generateRobotsTxt: true, // (optional)
-  outDir: "./out",
-  alternateRefs: [
-    {
-      href: "https://www.furrycons.cn",
-      hreflang: "zh-cn",
-    },
-    {
-      href: "https://cn.furryeventchina.com",
-      hreflang: "zh",
-    },
-  ],
-  // ...other options
+  generateRobotsTxt: true,
 };
