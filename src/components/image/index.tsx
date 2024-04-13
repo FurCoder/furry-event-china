38a2ac5297e0cc2778d78d29821cfa89a4ebdb6c
JiPai <3956400+PaiJi@users.noreply.github.com>
2024-04-14 00:22:50 +0800

    feat(image): support avif (#163)

--- src/components/image/index.tsx
+++ src/components/image/index.tsx
@@ -7,53 +7,115 @@ function Image({
   alt,
   sizes,
   className,
+  containerClassName,
   onLoadingComplete,
   quality,
   width,
   height,
   priority,
+  autoFormat,
 }: {
   id?: string;
   src: string;
   alt: string;
   className?: string;
+  containerClassName?: string;
   sizes?: string;
   onLoadingComplete?: (img: HTMLImageElement) => void;
   quality?: number;
   width?: number;
   height?: number;
   priority?: boolean;
+  autoFormat?: boolean;
 }) {
-  const regex = /\b(\d+)px\b(?![^(]*\))/g;
-  const getSrcset = () => {
-    const sizesResult = Array.from(new Set(sizes?.match(regex)));
-
-    return sizesResult
-      .map(
-        (w) =>
-          `${imageLoader({ src, quality, width: parseInt(w) })} ${parseInt(w)}w`
-      )
-      .join(", ");
-  };
-
   const srcString = width ? imageLoader({ src, quality, width }) : src;
 
   return (
-    <img
-      id={id}
-      className={className}
-      src={srcString}
-      alt={alt}
-      onLoad={(e) =>
-        onLoadingComplete && onLoadingComplete(e.target as HTMLImageElement)
-      }
-      sizes={sizes}
-      srcSet={getSrcset()}
-      loading={priority ? "eager" : "lazy"}
-      //@ts-ignore
-      fetchpriority={priority ? "high" : "auto"}
-    />
+    <picture className={containerClassName}>
+      {autoFormat && (
+        <ImgSources
+          avifsrcSet={getSrcset({ src, quality, sizes, avif: true })}
+          webpsrcSet={getSrcset({ src, quality, sizes, webp: true })}
+          sizes={sizes}
+        />
+      )}
+      <img
+        id={id}
+        className={className}
+        src={srcString}
+        alt={alt}
+        onLoad={(e) =>
+          onLoadingComplete && onLoadingComplete(e.target as HTMLImageElement)
+        }
+        sizes={sizes}
+        srcSet={getSrcset({ src, quality, sizes })}
+        loading={priority ? "eager" : "lazy"}
+        //@ts-ignore
+        fetchpriority={priority ? "high" : "auto"}
+      />
+    </picture>
   );
+}
+
+function ImgSources({
+  avifsrcSet,
+  webpsrcSet,
+  sizes,
+}: {
+  avifsrcSet?: string;
+  webpsrcSet?: string;
+  sizes?: string;
+}) {
+  return (
+    <>
+      {avifsrcSet && (
+        <source type="image/avif" srcSet={avifsrcSet} sizes={sizes} />
+      )}
+      {webpsrcSet && (
+        <source type="image/webp" srcSet={webpsrcSet} sizes={sizes} />
+      )}
+    </>
+  );
+}
+
+function getSrcset({
+  src,
+  quality,
+  sizes,
+  avif,
+  webp,
+}: {
+  src: string;
+  quality?: number;
+  sizes?: string;
+  avif?: boolean;
+  webp?: boolean;
+}) {
+  const regex = /\b(\d+)px\b(?![^(]*\))/g;
+
+  const sizesResult = Array.from(new Set(sizes?.match(regex)));
+
+  if (sizesResult.length) {
+    return sizesResult
+      .map(
+        (w) =>
+          `${imageLoader({
+            src,
+            quality,
+            width: parseInt(w),
+            avif,
+            webp,
+          })} ${parseInt(w)}w`
+      )
+      .join(", ");
+  } else {
+    return imageLoader({
+      src,
+      quality,
+      avif,
+      webp,
+    });
+  }
 }
 
 export default Image;
--- src/utils/imageLoader.ts
+++ src/utils/imageLoader.ts
@@ -26,22 +26,38 @@ export const imageUrl = (src: string) => {
   return `https://${GLOBAL_AUTO_CDN_IMAGE_URL}/${withoutDefaultHostSrc}`;
 };
 
-const imageLoader = ({ src, width, quality }: ImageLoaderProps) => {
+const imageLoader = ({
+  src,
+  width,
+  quality,
+  avif,
+  webp,
+}: Pick<ImageLoaderProps, "src" | "quality"> & {
+  avif?: boolean;
+  webp?: boolean;
+  width?: number;
+}) => {
   const withoutDefaultHostSrc = src
     .replace("https://cdn.furryeventchina.com/", "")
     .replace("https://images.furryeventchina.com/", "")
     .replace("https://images.furrycons.cn/", "")
     .trim();
 
-  if (process.env.NODE_ENV === "development") {
-    return `https://${GLOBAL_MANUAL_CDN_IMAGE_URL}/${withoutDefaultHostSrc}?w=${width}&q=${
-      quality || 75
-    }`;
-  }
+  const imageURLHost =
+    process.env.NODE_ENV === "development"
+      ? `https://${GLOBAL_MANUAL_CDN_IMAGE_URL}`
+      : `https://${GLOBAL_AUTO_CDN_IMAGE_URL}`;
+  const imageURL = new URL(`${imageURLHost}/${withoutDefaultHostSrc}`);
 
-  return `https://${GLOBAL_AUTO_CDN_IMAGE_URL}/${withoutDefaultHostSrc}?w=${width}&q=${
-    quality || 75
-  }`;
+  width !== undefined && imageURL.searchParams.set("w", width.toString());
+  imageURL.searchParams.set(
+    "q",
+    quality === undefined ? "75" : quality.toString()
+  );
+  avif && imageURL.searchParams.set("f", "avif");
+  webp && imageURL.searchParams.set("f", "webp");
+
+  return imageURL.toString();
 };
 
 export default imageLoader;
