e275c0355f5269c624b68f01d50fb4d47e27b8ea
JiPai <3956400+PaiJi@users.noreply.github.com>
2024-06-13 17:19:54 +0800

    fix: rollback GA4 install code (#194)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,9 @@
 import { Noto_Sans_SC, Rubik } from "next/font/google";
-import { GoogleAnalytics } from "@next/third-parties/google";
 
 import Layout from "@/components/layout";
 
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { isEnableTrack } from "./_document";
 
 const notoSC = Noto_Sans_SC({
   weight: ["400", "500", "700"],
@@ -36,7 +34,6 @@ export default function App({ Component, pageProps }: AppProps) {
       >
         <Component {...pageProps} />
       </Layout>
-      {isEnableTrack && <GoogleAnalytics gaId="G-RBND7XQ43D" />}
     </>
   );
 }
